084ed86a844fb0a6c63519e4916dfe26cf028eb4
Rahul <apkay23@gmail.com>
2017-09-14 18:40:31 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app=express();
+
+app.set('view engine','hbs');
+
+
+app.use((req,res,next) => {
+var now = new Date().toString();
+var log = `${now}: ${req.method} ${req.url}`
+fs.appendFile('server-log',log + '\n', (err)=>{
+  if(err){ console.log('Error while writing logs');}
+});
+next();
+});
+app.use((req,res,next)=>{
+  res.render('magnet.hbs');
+});
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+app.get('/',(req,res)=>{
+  // res.send('<h1>Hello express!<h1>');
+  res.render('home.hbs',{
+    pageTitle:'Home Page',
+    message:'Welcome to Express'
+  });
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle:'About Page',
+  });
+});
+
+app.get('/bad',(req,res) =>{
+  res.send({
+    errorMessage:404,
+  });
+});
+
+app.listen(3000,()=>{
+  console.log('We are live on port:3000');
+});
